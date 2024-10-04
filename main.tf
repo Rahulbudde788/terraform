4a23906d55c495044cf3672169ac1803d7fa9d60
Rahulbudde788 <rahulbudde788@gmail.com>
2024-10-04 15:19:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0fff1b9a61dec8a5f"
   instance_type = "t2.micro"
 }
